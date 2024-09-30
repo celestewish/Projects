fd3179de81aa3cbf9b3066125369b50b9037e952
Lauren Rousell <laurenrousell@outlook.com>
2024-09-30 09:43:14 -0400

    Update TestPalindrome.java

--- Queues/TestPalindrome.java
+++ Queues/TestPalindrome.java
@@ -1,9 +1,3 @@
-//Name: Lauren Rousell
-//Class: CS 3305/H01
-//Term: Fall 2024
-//Instructor: Dr. Haddad
-//Assignment: 5
-//IDE Name: IntelliJ
 import java.util.Scanner;
 //this program tests if a string is a palindrome in two different ways
 public class TestPalindrome {
